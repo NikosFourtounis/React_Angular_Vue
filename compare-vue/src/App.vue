b53699039864b96e3ec2098c92fedc29da9048f2
N Fourtounis <nickfortune@windowslive.com>
2018-10-18 19:37:13 +0300

    footer

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -59,6 +59,18 @@
       </div>
     </nav>
     <router-view/>
+    <footer class="footer is-primary">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Website</strong> by <a href="https://www.linkedin.com/in/nick-fourtounis-40b039116/">Nick Fourtounis</a>. For the source code click
+          <a href="http://opensource.org/licenses/mit-license.php">here</a>.
+        </p>
+      </div>
+      <div class="content has-text-right">
+        <a class="icon" href="#"><i class="fab fa-facebook-square"></i></a>
+        <a class="icon" href="#"><i class="fab fa-twitter"></i></a>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -115,4 +127,14 @@ a.navbar-link
     color: gray
     &:hover
       background-color: #F1F1F1
+a.navbar-burger 
+  color:#C1C1C1
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+  .icon
+    color: #fff
+    margin-left: 20px
 </style>
