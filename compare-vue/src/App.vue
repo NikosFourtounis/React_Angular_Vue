5203bd08bb6230aa6c22eedebae1425b85345bc6
N Fourtounis <nickfortune@windowslive.com>
2018-10-19 18:49:45 +0300

    Hero

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -18,10 +18,10 @@
             Home
           </a>
 
-          <a class="navbar-item" href="/faq">
+          <a class="navbar-item" >
             Features
           </a>
-          <a class="navbar-item">
+          <a class="navbar-item" href="/#/faq">
             FAQ
           </a>
           <div class="navbar-item has-dropdown is-hoverable">
--- compare-vue/src/components/Home.vue
+++ compare-vue/src/components/Home.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-light is-medium ">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{heading}}</h1>
+          
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{subheading}}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'My Website',
+      subheading:'This is a website build with Vue javascript framework and Bulma css framework.'
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='sass'>
 @import '../mq'
+
+.hero
+  background: url('../assets/header.jpg')
+  background-repeat: no-repeat
+  background-size: 300px 100px
+  background-size: cover
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
+    
 </style>
