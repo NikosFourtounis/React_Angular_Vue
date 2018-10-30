a9531b1f3015ac3df85a6fce07c2186c006b3dd9
N Fourtounis <nickfortune@windowslive.com>
2018-10-30 12:07:41 +0200

    Footer css+html

--- compare-react/src/components/Footer/Footer.js
+++ compare-react/src/components/Footer/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
       <div className="App">
-        My Footer
+        <footer class="footer is-primary">
+          <div class="content has-text-centered">
+            <p>
+              <strong>Website</strong> by <a href="https://www.linkedin.com/in/nick-fourtounis-40b039116/">Nick Fourtounis</a>. For the source code click
+              <a href="http://opensource.org/licenses/mit-license.php">here</a>.
+            </p>
+          </div>
+          <div class="content has-text-right">
+            <a class="icon" href="#"><i class="fab fa-facebook-square"></i></a>
+            <a class="icon" href="#"><i class="fab fa-twitter"></i></a>
+          </div>
+        </footer>
       </div>
     );
   };
